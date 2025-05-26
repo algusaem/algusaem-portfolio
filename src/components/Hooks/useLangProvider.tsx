4355893d56761aebdb822417719570490bf7df9f
Alex Gutierrez <a.gutierrez@orekait.com>
2025-05-26 17:17:17 +0200

    Use browser language by default

--- src/components/Hooks/useLangProvider.tsx
+++ src/components/Hooks/useLangProvider.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useEffect, useState } from "react";
 import i18n from "../../../i18n";
 
@@ -6,15 +8,26 @@ const useLangProvider = () => {
 
   useEffect(() => {
     const storedLang = localStorage.getItem("lang");
-    const initialLang = storedLang || "es";
-    setLang(initialLang);
-    i18n.changeLanguage(initialLang);
+
+    if (storedLang) {
+      setLang(storedLang);
+      return;
+    }
+
+    // Use the most preferred matching language
+    const preferredLang =
+      navigator.languages
+        ?.find((l) => ["es", "en"].includes(l.slice(0, 2)))
+        ?.slice(0, 2) || "es";
+
+    console.log("Detected browser-preferred lang:", preferredLang);
+    setLang(preferredLang);
   }, []);
 
   useEffect(() => {
     if (lang) {
-      localStorage.setItem("lang", lang);
       i18n.changeLanguage(lang);
+      localStorage.setItem("lang", lang);
     }
   }, [lang]);
 
